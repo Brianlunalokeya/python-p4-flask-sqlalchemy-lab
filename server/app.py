7c174e40414726a240817007fee1aeba3dc25218
+ <+>
2023-06-27 05:50:16 +0300

    Done with Assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -19,16 +17,65 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        response_body = f'''
+            <h1>{animal.name}</h1>
+            <h2>Species: {animal.species}</h2>
+            <h3>Zookeeper: {animal.zookeeper.name}</h3>
+            <h3>Enclosure: {animal.enclosure.environment}</h3>
+        '''
+        status_code = 200
+    else:
+        response_body = '<h1>Animal not found</h1>'
+        status_code = 404
+
+    headers = {}
+    return make_response(response_body, status_code, headers)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        animals = zookeeper.animals
+        response_body = f'''
+            <h1>{zookeeper.name}</h1>
+            <h2>Birthday: {zookeeper.birthday}</h2>
+            <h3>Animals:</h3>
+            <ul>
+        '''
+        for animal in animals:
+            response_body += f'<li>{animal.name} - {animal.species}</li>'
+        response_body += '</ul>'
+        status_code = 200
+    else:
+        response_body = '<h1>Zookeeper not found</h1>'
+        status_code = 404
+
+    headers = {}
+    return make_response(response_body, status_code, headers)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        animals = enclosure.animals
+        response_body = f'''
+            <h1>Enclosure: {enclosure.environment}</h1>
+            <h2>Open to Visitors: {enclosure.open_to_visitors}</h2>
+            <h3>Animals:</h3>
+            <ul>
+        '''
+        for animal in animals:
+            response_body += f'<li>{animal.name} - {animal.species}</li>'
+        response_body += '</ul>'
+        status_code = 200
+    else:
+        response_body = '<h1>Enclosure not found</h1>'
+        status_code = 404
 
+    headers = {}
+    return make_response(response_body, status_code, headers)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,17 +7,21 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
+class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    species = db.Column(db.String(50))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'))
+
+class Zookeeper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    birthday = db.Column(db.Date)
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
 
 class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
     id = db.Column(db.Integer, primary_key=True)
-
-class Animal(db.Model):
-    __tablename__ = 'animals'
-
-    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(50))
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
